84767bfdff93246b30c421e8b79c5a2cf22372c9
Eugenio Caruso <ecaruso@student.42roma.it>
2023-05-12 20:09:21 +0200

    hello

--- put_img2.c
+++ put_img2.c
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:19:43 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/05/12 18:36:06 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/05/12 20:07:58 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	put_c_img(t_game *game, int y, int x)
 	mlx_put_image_to_window (game->graphics.mlx_pointer,
 		game->graphics.mlx_window, game->image.c, x * 64, (y + 1) * 64);
 }
+
 void	put_d_img(t_game *game, int y, int x)
 {
 	mlx_put_image_to_window (game->graphics.mlx_pointer,
--- stab_animation.c
+++ stab_animation.c
@@ -6,15 +6,11 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:36:08 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/05/12 19:37:40 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/05/12 20:08:43 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <unistd.h>
-#include <time.h>
-#include <stdio.h>
-
 
 void	stab(t_game *game)
 {
@@ -39,34 +35,37 @@ void	stab_r(t_game *game)
 		game->map.map_array[game->pcoords.y][game->pcoords.x + 1] = 'D';
 	scan_map(game);
 }
+
 void	stab_l(t_game *game)
 {
 	mlx_clear_window(game->graphics.mlx_pointer, game->graphics.mlx_window);
 	game->image.p = mlx_xpm_file_to_image (game->graphics.mlx_pointer,
-		"./images/stab_l.xpm", &game->map.map_width,
-		&game->map.map_height);
+			"./images/stab_l.xpm", &game->map.map_width,
+			&game->map.map_height);
 	put_player_img(game, game->pcoords.y, game->pcoords.x);
 	if (game->map.map_array[game->pcoords.y][game->pcoords.x - 1] == 'N')
 		game->map.map_array[game->pcoords.y][game->pcoords.x - 1] = 'D';
 	scan_map(game);
 }
+
 void	stab_d(t_game *game)
 {
 	mlx_clear_window(game->graphics.mlx_pointer, game->graphics.mlx_window);
 	game->image.p = mlx_xpm_file_to_image (game->graphics.mlx_pointer,
-		"./images/stab_d.xpm", &game->map.map_width,
-		&game->map.map_height);
+			"./images/stab_d.xpm", &game->map.map_width,
+			&game->map.map_height);
 	put_player_img(game, game->pcoords.y, game->pcoords.x);
 	if (game->map.map_array[game->pcoords.y + 1][game->pcoords.x] == 'N')
 		game->map.map_array[game->pcoords.y + 1][game->pcoords.x] = 'D';
 	scan_map(game);
 }
+
 void	stab_u(t_game *game)
 {
 	mlx_clear_window(game->graphics.mlx_pointer, game->graphics.mlx_window);
 	game->image.p = mlx_xpm_file_to_image (game->graphics.mlx_pointer,
-		"./images/stab_u.xpm", &game->map.map_width,
-		&game->map.map_height);
+			"./images/stab_u.xpm", &game->map.map_width,
+			&game->map.map_height);
 	put_player_img(game, game->pcoords.y, game->pcoords.x);
 	if (game->map.map_array[game->pcoords.y - 1][game->pcoords.x] == 'N')
 		game->map.map_array[game->pcoords.y - 1][game->pcoords.x] = 'D';
